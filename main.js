c3bfce1f526bb36ac5ba5eeec86b194de63542a9
dhpcode <davrsirius@gmail.com>
2026-01-20 19:06:10 +0500

    Corrected url

--- main.js
+++ main.js
@@ -10,15 +10,15 @@ console.log(randomNumber1, randomNumber2);
 
 
 if (randomNumber1 > randomNumber2) {
-  player1Image.src = `../images/dice-${randomNumber1}.png`;
-  player2Image.src = `../images/dice-${randomNumber2}.png`;
+  player1Image.src = `./images/dice-${randomNumber1}.png`;
+  player2Image.src = `./images/dice-${randomNumber2}.png`;
   result.textContent = `Player 1 wins!!!`
 } else if (randomNumber1 < randomNumber2) {
-  player1Image.src = `../images/dice-${randomNumber1}.png`;
-  player2Image.src = `../images/dice-${randomNumber2}.png`;
+  player1Image.src = `./images/dice-${randomNumber1}.png`;
+  player2Image.src = `./images/dice-${randomNumber2}.png`;
   result.textContent = `Player 2 wins!!!`
 } else {
-  player1Image.src = `../images/dice-${randomNumber1}.png`;
-  player2Image.src = `../images/dice-${randomNumber2}.png`;
+  player1Image.src = `./images/dice-${randomNumber1}.png`;
+  player2Image.src = `./images/dice-${randomNumber2}.png`;
   result.textContent = `Draw 🤝`
 }
